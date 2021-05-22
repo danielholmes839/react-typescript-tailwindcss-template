c1ebb4bbc6d98b0d697fc2a32c1af2bc8c26f2b3
Daniel <danielh839@gmail.com>
2021-05-22 13:17:51 -0400

    Updates

---
+++ static/js/main.9d1a3e7b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-app"]=this["webpackJsonpreact-app"]||[]).push([[0],{10:function(t,e,c){},11:function(t,e,c){"use strict";c.r(e);var n=c(1),a=c.n(n),r=c(3),s=c.n(r),i=c(0),o=function(){return Object(i.jsx)("div",{className:"container mx-auto",children:Object(i.jsx)("h1",{className:"text-6xl",children:"React App"})})};c(9),c(10);s.a.render(Object(i.jsx)(a.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},9:function(t,e,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.9d1a3e7b.chunk.js.map
